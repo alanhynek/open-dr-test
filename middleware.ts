dcfa6d499d57be35edbbb4507f354108b314e389
alanhynek <79110454+alanhynek@users.noreply.github.com>
2025-02-10 20:57:08 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 
-import { authConfig } from '@/app/(auth)/auth.config.ts';
+import { authConfig } from '@/app/(auth)/auth.config';
 
 export default NextAuth(authConfig).auth;
 
